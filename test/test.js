99b6688035e925e92164fb82751e71725d7f26c7
Raúl <Raul@samsung-pc.com>
2017-10-28 20:18:29 +0200

    Added test frameworks

---
+++ test/test.js
@@ -0,0 +1,21 @@
+// var assert = require('assert');
+// const expect = require('chai').expect;
+
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal(-1, [1,2,3].indexOf(4));
+//     });
+//   });
+// });
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1,2,3].indexOf(4)).to.equal(-1);
+    });
+
+    // pending test below
+    it('should test something...');
+  });
+});
